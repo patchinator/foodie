f42e56fb19e82c97cb5dfa4788a79ce95ce9b019
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-10-14 16:01:51 +0100

    created modal structure for form submission

--- components/postForm.js
+++ components/postForm.js
@@ -1,14 +1,57 @@
-import { Container, Box, Button, Flex } from "@chakra-ui/react";
+import { Fragment } from "react";
+
+import {
+  Container,
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Textarea,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/hooks";
 
 const PostForm = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Container>
-      <Flex justify="center" align="center" mt="10">
-        <Box>
-          <Button>What are you cooking...?</Button>
-        </Box>
-      </Flex>
-    </Container>
+    <Fragment>
+      <Container>
+        <Flex justify="center" align="center" mt="10">
+          <Box>
+            <Button onClick={onOpen}>What are you cooking...?</Button>
+          </Box>
+        </Flex>
+      </Container>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign="center">
+            What you cooking good looking
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Tell us something</FormLabel>
+              <Textarea placeholder="I am cooking..."></Textarea>
+              <FormHelperText>150 chars max</FormHelperText>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr="2">Post</Button>
+            <Button>Add image</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Fragment>
   );
 };
 
